ab59df135d78a3f3fd60e7f069b852ffd969e416
Thiago <thiagojara3@gmail.com>
2025-04-05 15:49:48 -0300

    mas respuestas para el proyecto

--- TP Trabajo colaborativo.py
+++ TP Trabajo colaborativo.py
@@ -5,9 +5,9 @@
 
 Git es una plataforma en  linea que se utiliza para trabajos colaborativos en los que se puede compartirar y guardar proyectos de software, es como una red social pero se suben proyectos de programacion.
 
-#¿Cómo crear un repositorio en GitHub? Primero vas a la pagina de github.com, inicias sesion, vas al boton "NEW", nombras el repositorio como prefieras, clickeas en "create repository" luego en la consola git bash colocas "git init" para iniciar un repositorio en github.
+#¿Cómo crear un repositorio en GitHub? 
 
-luego con el comando "git add ." para agregar todos los archivos y carpetas para que git registre sus cambios, luego con el comando "commit -m" para registrar los cambios y despues tener puntos donde volver atras y ver cuando se hicieron lso cambios en el proyecto con un mensaje, tambien tener que configurar la ruta de usuario usanto los comandos "git config --global user.email (mail)" y el comando "git config --global user.name (nombre)", luego copias el comando proportcionado en la pagina de github una vez creado el repositorio para vincular el archivo.
+Primero vas a la pagina de github.com, inicias sesion, vas al boton "NEW", nombras el repositorio como prefieras, clickeas en "create repository" luego en la consola git bash colocas "git init" para iniciar un repositorio en github.
 
 #¿Cómo crear una rama en Git? 
 
@@ -19,5 +19,34 @@ Nos posicionamos en la rama principal y luego con el comando "git merge (nombre 
 
 #¿Cómo crear un commit en Git?
 
-Primero agregamos todos los archivos modificados con el comando "git add ." luego creamos el commit con el comando "git commit -m (nombre para el commit)" y para subir el archivo a git tenes que colocar el comando "git push origin (nombre de la rama)"
+Primero agregamos todos los archivos modificados con el comando "git add ." luego creamos el commit con el comando "git commit -m (nombre para el commit)"
 
+#¿Cómo enviar un commit a GitHub? 
+
+Para subir el archivo a git tenes que colocar el comando "git add ." para agregar todos los archivos modificados, luego con el comando "commit -m" para registrar los cambios y despues tener puntos donde volver atras y ver cuando se hicieron lso cambios en el proyecto con un mensaje, luego usar el comando "git push origin master" para subir el commit en github.
+
+#¿Qué es un repositorio remoto? 
+
+Es un lugar donde guardar una copia de un proyecto de software, es una version guardada en una nube como github.
+
+#¿Cómo agregar un repositorio remoto a Git? 
+
+Se agregan con el comando "git remote add origin url"
+
+#¿Cómo empujar cambios a un repositorio remoto? 
+
+Se empujan los cambios con el comando "git push -u origin master"
+
+#¿Cómo tirar de cambios de un repositorio remoto?
+
+Se tiran cmabios con el comando "git fetch"
+
+#¿Qué es un fork de repositorio? 
+
+Un fork es una copia del repositorio de otra persona en tu cuenta de GitHub
+
+#¿Cómo crear un fork de un repositorio? 
+
+Ingresas al repositorio que queres hacer Fork, luego clickeas en la opcion "fork", elegis si es en tu cuenta o una organizacion y listo. 
+
+#¿Cómo enviar una solicitud de extracción (pull request) a un repositorio? 
